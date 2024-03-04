fd14fb3ac4b76ca63c609f1972a0e90ead1acc9b
hidelrios <hidels.rios@gmail.com>
2024-03-04 09:11:12 -0300

    start project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { AppProps } from "next/app";
+import { globalStyles } from "../styles/global";
+
+import logoImg from "../assets/logo.svg";
+import { Container, Header } from "../styles/pages/app";
+import Image from "next/image";
+globalStyles();
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
+}
+
+export default App;
