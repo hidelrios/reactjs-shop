fc1414358261333f2bbf6da24479e157578a1fde
hidelrios <hidels.rios@gmail.com>
2024-03-04 18:00:39 -0300

    feat: dados de compra no sucesso

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -1,7 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { stripe } from "../../lib/stripe";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { priceId } = req.body;
 
   if (req.method !== "POST") {
@@ -9,23 +12,23 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 
   if (!priceId) {
-    return res.status(400).json({ error: 'Price not found.' });
+    return res.status(400).json({ error: "Price not found." });
   }
 
-  const successUrl = `${process.env.NEXT_URL}/success`;
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
   const cancelUrl = `${process.env.NEXT_URL}/`;
   const checkoutSession = await stripe.checkout.sessions.create({
     success_url: successUrl,
     cancel_url: cancelUrl,
-    mode: 'payment',
+    mode: "payment",
     line_items: [
       {
         price: priceId,
         quantity: 1,
-      }
-    ]
-  })
+      },
+    ],
+  });
   return res.status(201).json({
-    checkoutUrl: checkoutSession.url
-  })
+    checkoutUrl: checkoutSession.url,
+  });
 }
